47cbba591ef401659ba0a21077dc43ccebe05842
Pavel Murat <murat@fnal.gov>
2023-10-14 16:28:25 -0500

    handle multiple ROCs

--- mod/mod/TrkFragmentAna_module.hh
+++ mod/mod/TrkFragmentAna_module.hh
@@ -47,6 +47,7 @@ namespace mu2e {
   class TrkFragmentAna : public THistModule {
 
     enum { kNChannels          = 96,
+           kMaxNLinks          =  6,
            kMaxNHitsPerChannel = 20
     };
 
@@ -80,38 +81,57 @@ namespace mu2e {
     int              _minNBytes;
     int              _maxNBytes;
     int              _dataHeaderOffset;
+    std::vector<int> _activeLinks;            // active links - connected ROCs
+    int              _nActiveLinks;
 
     art::InputTag    _trkfCollTag;
     int              _dumpDTCRegisters;
-    int              _referenceChannel[2];
+    int              _referenceChannel[kMaxNLinks][2];
     int              _analyzeFragments;
+    int              _maxFragmentSize;
     
-    int              _error; 
-
     int              _adc_index_0 [kNChannels]; // seq num of the channel 'i' in the readout sequence
     int              _adc_index_1 [kNChannels]; // fixed map, seq num of the channel 'i' in the readout sequence
     double           _gen_offset  [kNChannels];
 
-    double           _freq;        // generator frequency, defined by the run number
-    double           _dt;          // expected distance between the two pulses
-    double           _time_window; // time window (tistance between the two EWMs for a given run
+    double           _freq;           // generator frequency, defined by the run number
+    double           _dt;             // expected distance between the two pulses
+    double           _time_window;    // time window (tistance between the two EWMs for a given run
+    double           _tdc_bin;        // 
+    double           _tdc_bin_ns;     // TDC bin, in nanoseconds
 
+    struct ChannelHist_t {
+      TH1F*         nhits;
+      TH1F*         time[2];
+      TH1F*         t0  [2];            // early times in ns
+      TH1F*         t1  [2];            // late  times in ns
+      TH1F*         tot [2];
+      TH1F*         pmp;
+      TH1F*         dt0;                // T0 distance between the two consequtive pulses
+      TH1F*         dt1;                // T1 distance between the two consequtive pulses
+      TH1F*         dt2;                // T2 = (dt1+dt2)/2
+      TH1F*         dt0r;               // T0(ich,0)-T0(ref,0)
+      TH1F*         dt1r;               // T1(ich,0)-distance between the two pulses (if more than one)
+      TH1F*         wf[kMaxNHitsPerChannel];
+    };
+                                        // per-ROC histograms
     struct EventHist_t {
       TH1F*         nbtot;
       TH1F*         nfrag;
       TH1F*         nhits;
-      TH1F*         nhits_vs_ich;
-      TH1F*         nhits_vs_adc[2];
+      TH1F*         fsize;
       TH1F*         error;
+      TH1F*         valid;
     };
-                                        // per-ROC histograms
-    struct FragmentHist_t {
+
+    struct RocHist_t {
       TH1F*         nbytes;
-      TH1F*         dsize;              // size()-nbytes
+      // TH1F*         dsize;              // size()-nbytes
       TH1F*         npackets;
       TH1F*         nhits;
       TH1F*         valid;
       TH2F*         nh_vs_ch;
+      TH2F*         nh_vs_adc1;
 
       TH2F*         dt0r_vs_ch;
       TH2F*         dt1r_vs_ch;
@@ -125,43 +145,58 @@ namespace mu2e {
 
       TH2F*         dt0rc_vs_adc[2];
       TH2F*         dt1rc_vs_adc[2];
-    };
-                                        // assume one panel
-    struct ChannelHist_t {
-      TH1F*         nhits;
-      TH1F*         time[2];
-      TH1F*         t0  [2];             // early times in ns
-      TH1F*         t1  [2];             // late  times in ns
-      TH1F*         tot [2];
-      TH1F*         pmp;
-      TH1F*         dt0;                 // T0 distance between the two consequtive pulses
-      TH1F*         dt1;                 // T1 distance between the two consequtive pulses
-      TH1F*         dt2;                 // T2 = (dt1+dt2)/2
-      TH1F*         dt0r;                // T0(ich,0)-T0(ref,0)
-      TH1F*         dt1r;                // T1(ich,0)-distance between the two pulses (if more than one)
-      TH1F*         wf[kMaxNHitsPerChannel];
-    };
-                                        // assume one panel
-    struct WaveformHist_t {
-      ChannelHist_t     channel[kNChannels];
-    };
 
+      TH1F*         nhits_vs_ich;
+      TH1F*         nhits_vs_adc[2];
+
+      ChannelHist_t channel[kNChannels];
+    };
+//-----------------------------------------------------------------------------
+// forgetting, for now, about multiple DTC's
+//-----------------------------------------------------------------------------
     struct Hist_t {
-      EventHist_t       event;
-      FragmentHist_t    frag;
-      ChannelHist_t     channel[kNChannels];  // so far, assume one panel
+      EventHist_t   event;
+      RocHist_t     roc[kMaxNLinks];
     } _Hist;
 
-    int _nwf[100];
-
-    struct ChData_t {
-      int  nhits;
-      TrackerFragment::DataPacket* hit[100];
+    struct ChannelData_t {
+      int      nhits;
+      float    dt0r;                     // time dist btw this channel and an FPGA reference channel, TDC0, ns
+      float    dt1r;                     // time dist btw this channel and an FPGA reference channel, TDC1, ns
+      float    dt0r_c;                   // the same, corrected for the FPGA-specific generator time offset
+      float    dt1r_c;
+      
+      TrackerFragment::DataPacket* hit[kMaxNHitsPerChannel];
     };
 
-    struct Data_t {
-      ChData_t  channel[kNChannels];
-    } _data;
+    struct RocData_t {
+      int       size;
+      int       nhits;
+      int       nbytes;
+      int       npackets;
+      int       valid;
+      
+      ChannelData_t  channel[kNChannels];
+      ChannelData_t* ref_ch [2];
+
+      float     dt0r01;                 // time difference between the two reference channels, TDC0, ns
+      float     dt1r01;                 // time difference between the two reference channels, TDC1, ns
+    };
+
+    struct FragmentData_t {
+      int       nbytes;
+    };
+                                        // in reality, this is fragment data, could be multiple fragments
+    struct EventData_t {
+      int       nbtot;                  // total nbytes
+      int       nhtot;
+      int       nfrag;
+      int       valid;
+      int       error;
+      RocData_t rdata[kMaxNLinks];
+
+      std::vector<FragmentData_t> fragments;
+    } _event_data;
 
     // struct Config {
     //   fhicl::Atom<int>               diagLevel{fhicl::Name("diagLevel"), fhicl::Comment("diagnostic level")};
@@ -180,8 +215,19 @@ namespace mu2e {
     virtual void endJob  () override;
     
     virtual void analyze         (const art::Event& e) override;
-    void         analyze_fragment(const artdaq::Fragment* Fragment,FragmentHist_t* Hist);
-    void         book_histograms (int RunNumber);
+    void         analyze_fragment(const art::Event& e, const artdaq::Fragment* Fragment);
+
+    void         book_channel_histograms(art::TFileDirectory* Dir, int RunNumber, ChannelHist_t* Hist, int Ich);
+    void         book_event_histograms  (art::TFileDirectory* Dir, int RunNumber, EventHist_t*   Hist);
+    void         book_roc_histograms    (art::TFileDirectory* Dir, int RunNumber, RocHist_t*     Hist, int Link);
+    void         book_histograms        (int RunNumber);
+  
+    void         fill_channel_histograms(ChannelHist_t* Hist, ChannelData_t* Data);
+    void         fill_event_histograms  (EventHist_t*   Hist, EventData_t*   Data);
+    void         fill_roc_histograms    (RocHist_t*     Hist, RocData_t*     Data);
+
+                                        // returns -1 if in trouble
+    int          fill_histograms        ();
 
     // NWords: number of 2-byte words
     void         printFragment   (const artdaq::Fragment* Fragment, int NWords);
